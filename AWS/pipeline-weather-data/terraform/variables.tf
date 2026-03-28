fd16751c6dca8d778141fbcb8975e422a84bc150
apeksha <aherape@gmail.com>
2026-03-28 18:01:35 +0530

    account id and git cred

--- AWS/pipeline-weather-data/terraform/variables.tf
+++ AWS/pipeline-weather-data/terraform/variables.tf
@@ -1,19 +1,19 @@
 variable "github_owner" {
   type        = string
   description = "GitHub repository owner (username or organization)"
-  default     = "claudiocmm"
+  default     = "Apekshaaher9"
 }
 
 variable "github_repo" {
   type        = string
   description = "GitHub repository name"
-  default     = "data_engineering_projects_temp"
+  default     = "data_engineering_projects"
 }
 
 variable "account_id" {
   type        = string
   description = "Account Id AWS"
-  default     = "xxxxxxxxx" // Replace with your account id
+  default     = "603306868045" // Replace with your account id
 }
 
 variable "region" {
