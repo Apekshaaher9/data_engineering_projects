95fb5eb8d3ac19a0b1bb53d61be87a95bbbe93df
apeksha <aherape@gmail.com>
2026-03-29 08:33:58 +0530

    snowflake id

--- AWS/pipeline-weather-data/terraform/variables.tf
+++ AWS/pipeline-weather-data/terraform/variables.tf
@@ -33,24 +33,3 @@ variable "pipeline_name" {
   description = "Name of pipeline"
   default     = "pipeline-weather-data"
 }
-
-
-///////// SNOWFLAKE VARIABLES CONNECTIONS /////////////////
-
-//variable "aws_iam_user_arn_by_snowflake" {
-//  type        = string
-//  description = "Iam user ARN by snowflake"
-//  default     = "arn:aws:iam::xxxxxxxxx:user/yubx0000-s" // Replace with your Iam user ARN by snowflake
-//}
-
-///variable "glue_aws_external_id_by_snowflake" {
-///  type        = string
-///  description = "Glue aws external id by snowflake"
-///  default     = "xxxxxxxxxxxxxxxxxxx" // Replace with your Glue aws external id by snowflake
-///}
-
-///variable "storage_aws_external_id_by_snowflake" {
-///  type        = string
-///  description = "Storage aws external id by snowflake"
-///  default     = "xxxxxxxxxxxxxxxxxxx" // Replace with your Storage aws external id by snowflake
-///}
